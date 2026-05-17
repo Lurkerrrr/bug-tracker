9226b7054639c91631e530622285787613bb5eaa
debugger <meganesh2005@gmail.com>
2026-05-18 00:09:38 +0200

    chore: replace Hello World scaffolding with /health endpoint, remove AppService

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import general from './config/general';
 import typeorm from './config/typeorm';
@@ -39,8 +38,6 @@ import { validate } from './config/env.validation';
     TicketsModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-  ],
+  providers: [],
 })
 export class AppModule { }

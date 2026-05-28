453ce5110b648aae28ccd1386ee7616c316b58c5
debugger <meganesh2005@gmail.com>
2026-05-28 21:37:35 +0200

    feat(team): add team route constant

--- frontend/src/shared/constants/routes.constants.ts
+++ frontend/src/shared/constants/routes.constants.ts
@@ -3,6 +3,7 @@ export const ROUTES = {
     REGISTER: '/register',
     BOARD: '/board',
     BACKLOG: '/backlog',
+    TEAM: '/team',
     TICKET: '/board/ticket/:id',
     COMING_SOON: '/coming-soon',
     SETTINGS: '/settings',
